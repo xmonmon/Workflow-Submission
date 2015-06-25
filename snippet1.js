b4921bfbbc00fa847d7fb2c2e490aa8e7aeff0ca
Monika Chumber <monikachumber@Monikas-MacBook-Pro.local>
2015-06-24 20:37:12 -0700

    again

--- snippet1.js
+++ snippet1.js
@@ -10,17 +10,3 @@ else if (beer === 0){
 }
 }
 
-var userLogin = {
-   userName: "Monika",
-   password: "12234"
-}
-var password;
-
-password = prompt("I sorta need you to enter your password now...");
-if (password === userLogin["password"]){
-   console.log("Passwords Match!")
-}
-else {
-   console.log("WELP, that password was SO wrong. TRY AGAIN!")
-}
-
